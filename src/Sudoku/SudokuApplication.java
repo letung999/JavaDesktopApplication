02a9dac5b487276bbac982bde073406db8c58eff
Your Name <you@example.com>
2020-10-01 21:03:54 +0700

    first commit

---
+++ src/Sudoku/SudokuApplication.java
@@ -0,0 +1,28 @@
+package Sudoku;
+
+import Sudoku.buildlogic.SudokuBuildLogic;
+import Sudoku.userinterface.IUserInterfaceContract;
+import Sudoku.userinterface.UserInterfaceImpl;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Sudoku/constants/Message.java
@@ -0,0 +1,7 @@
+package Sudoku.constants;
+
+public class Message{
+    public static final String GAME_COMPLETE ="congratulation, you have won! New game?";
+    public static final String ERROR ="An error has occurred";
+
+}
